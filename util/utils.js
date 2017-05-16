f250aa18f1d9e8f494b3d577c01cd0c586214252
Sebastian Hancock <sebastian.hancock@uky.edu>
2017-05-16 14:20:13 -0400

    updates

--- util/utils.js
+++ util/utils.js
@@ -1,41 +1,48 @@
 function handleFileSelect(evt) {
-    evt.stopPropagation();
-    evt.preventDefault();
+  evt.stopPropagation();
+  evt.preventDefault();
 
-    var files = evt.dataTransfer.files; // FileList object.
+  var files = evt.dataTransfer.files; // FileList object.
 
-    // files is a FileList of File objects. List some properties.
-    var output = [];
-    for (var i = 0, f; f = files[i]; i++) {
-        output.push(f.name, f.type, f.size);
-        console.log(files);
-    }
+  // files is a FileList of File objects. List some properties.
+  var output = [];
+  for (var i = 0, f; f = files[i]; i++) {
+    output.push(f.name, f.type, f.size);
+  }
 
-    var fr = new FileReader();
+  document.getElementById('list').innerHTML = output; //.join('');
 
-    fr.onload = function(e) {
-      console.log(e);
-      var result = JSON.parse(e.target.result);
-      var formatted = JSON.stringify(result, null, 2);
-      console.log(formatted);
-    };
+  var fr = new FileReader();
 
-    fr.readAsText(files[0]);
+  fr.readAsText(files[0]);
 
-    document.getElementById('list').innerHTML = output;//.join('');
+  fr.onload = function(e) {
+    //console.log(e);
+    var result = JSON.parse(e.target.result);
+    var formatted = JSON.stringify(result, null, 2);
+    console.log(result);
+    console.log('----------------------');
+    console.log(e.target.result);
+    console.log('----------------------');
+    console.log(formatted);
+  };
+
+
+
+
 }
 
 function handleDragOver(evt) {
-    evt.stopPropagation();
-    evt.preventDefault();
-    evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
-    $("#drop_zone").css("background-color", '#AA0000');
+  evt.stopPropagation();
+  evt.preventDefault();
+  evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+  $("#drop_zone").css("background-color", '#AA0000');
 }
 
 function handleDragLeave(evt) {
 
-    evt.stopPropagation();
-    evt.preventDefault();
-    $("#drop_zone").css("background-color", '');
+  evt.stopPropagation();
+  evt.preventDefault();
+  $("#drop_zone").css("background-color", '');
 
 }
