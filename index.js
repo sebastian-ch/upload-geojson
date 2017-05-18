488d5c3cf5840d2f127e16715edf5672cabb1782
Sebastian Hancock <sebastian.hancock@uky.edu>
2017-05-18 00:06:13 -0400

    updates

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
+var fs = require('fs');
 var express = require('express');
+/*var multer = require('multer');
+var upload = multer({
+  dest: 'uploads/'
+}); */
+var bodyParser = require('body-parser');
+//var geojsonhint = require('@mapbox/geojsonhint');
+
 var app = express();
+
+app.use(bodyParser.json({
+  limit: '50mb'
+}));
+
+app.use(bodyParser.urlencoded({
+  limit: '50mb',
+  extended: true,
+  parameterLimit: 1000000
+}));
 
 //local script
 app.use("/util", express.static(__dirname + "/util"));
+app.use("/uploads", express.static(__dirname + "/uploads"));
 
 app.set('view engine', 'ejs');
 
@@ -10,6 +29,31 @@ app.listen(3000, function() {
   console.log('listening on 3000');
 });
 
-app.get('/', function(rew, res) {
+app.get('/', function(req, res) {
   res.render('index.ejs');
 });
+
+app.post('/upload', function(req, res) {
+
+  var file = JSON.stringify(req.body.fileInput);
+  var name = req.body.fileName;
+
+  console.log(JSON.stringify(req.body.fileName));
+/////
+/////successfully saves input as JSON
+/////
+  /*saveInputAsJson(file, name, function(err) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+  }); */
+  res.redirect('/');
+  res.end();
+
+});
+
+function saveInputAsJson(file, name, callback) {
+  var outputName = __dirname + '/uploads/' + name;
+  fs.writeFile(outputName, file, callback);
+}
--- util/utils.js
+++ util/utils.js
@@ -1,41 +1,77 @@
 function handleFileSelect(evt) {
-    evt.stopPropagation();
-    evt.preventDefault();
+  evt.stopPropagation();
+  evt.preventDefault();
 
-    var files = evt.dataTransfer.files; // FileList object.
+  var files = evt.dataTransfer.files, // FileList object.
+    file_ext,
+    ref,
+    file_type,
+    // get name, type, size
+    output = [];
 
-    // files is a FileList of File objects. List some properties.
-    var output = [];
-    for (var i = 0, f; f = files[i]; i++) {
-        output.push(f.name, f.type, f.size);
-        console.log(files);
+  for (var i = 0, f; f = files[i]; i++) {
+    file_type = f.type;
+    if (file_type === '') {
+      ref = f.name.split('.');
+      file_ext = ref[ref.length - 1];
+      if (file_ext === 'json') {
+        file_type = 'application/json';
+      }
     }
 
-    var fr = new FileReader();
+    output.push(f.name, file_type, f.size);
+  }
 
-    fr.onload = function(e) {
-      console.log(e);
-      var result = JSON.parse(e.target.result);
+  //read file and send to server
+  var fr = new FileReader();
+  fr.readAsText(files[0]);
+  fr.onload = function(e) {
+    //console.log(e.target.result);
+    var result = e.target.result;
+    //var result = JSON.parse(e.target.result);
+    var validatGeoJson = geojsonhint.hint(result);
+    if (validatGeoJson.length > 0) {
+      var message = validatGeoJson.map(function(error) {
+        return 'Line ' + error.line + ': ' + error.message;
+      });
+      $('#error-header').show();
+      $('#error-message').html(message);
+    } else {
       var formatted = JSON.stringify(result, null, 2);
-      console.log(formatted);
-    };
+      console.log(result);
 
-    fr.readAsText(files[0]);
-
-    document.getElementById('list').innerHTML = output;//.join('');
+      $.ajax({
+        type: 'POST',
+        url: '/upload',
+        json: true,
+        data: {
+          'fileName': output[0],
+          'fileInput': result
+        },
+        success: function(data) {
+          console.log(result);
+          console.log('upload successful!');
+        }
+      });
+    }
+    document.getElementById('list').innerHTML = output.join(',<br> ');
+  };
 }
 
 function handleDragOver(evt) {
-    evt.stopPropagation();
-    evt.preventDefault();
-    evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
-    $("#drop_zone").css("background-color", '#AA0000');
+  evt.stopPropagation();
+  evt.preventDefault();
+  evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+  $("#drop_zone").css("background-color", '#AA0000');
 }
 
 function handleDragLeave(evt) {
 
-    evt.stopPropagation();
-    evt.preventDefault();
-    $("#drop_zone").css("background-color", '');
+  evt.stopPropagation();
+  evt.preventDefault();
+  $("#drop_zone").css("background-color", '');
+}
+
+function addInputToMap(result) {
 
 }
