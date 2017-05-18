8a074569ba944a15e14351e11121473f52cd316c
Sebastian Hancock <sebastian.hancock@uky.edu>
2017-05-18 00:28:43 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	package.json
    #	util/utils.js
    #	views/index.ejs

--- util/utils.js
+++ util/utils.js
@@ -2,6 +2,7 @@ function handleFileSelect(evt) {
   evt.stopPropagation();
   evt.preventDefault();
 
+<<<<<<< HEAD
   var files = evt.dataTransfer.files, // FileList object.
     file_ext,
     ref,
@@ -56,6 +57,36 @@ function handleFileSelect(evt) {
     }
     document.getElementById('list').innerHTML = output.join(',<br> ');
   };
+=======
+  var files = evt.dataTransfer.files; // FileList object.
+
+  // files is a FileList of File objects. List some properties.
+  var output = [];
+  for (var i = 0, f; f = files[i]; i++) {
+    output.push(f.name, f.type, f.size);
+  }
+
+  document.getElementById('list').innerHTML = output; //.join('');
+
+  var fr = new FileReader();
+
+  fr.readAsText(files[0]);
+
+  fr.onload = function(e) {
+    //console.log(e);
+    var result = JSON.parse(e.target.result);
+    var formatted = JSON.stringify(result, null, 2);
+    console.log(result);
+    console.log('----------------------');
+    console.log(e.target.result);
+    console.log('----------------------');
+    console.log(formatted);
+  };
+
+
+
+
+>>>>>>> origin/master
 }
 
 function handleDragOver(evt) {
@@ -70,8 +101,11 @@ function handleDragLeave(evt) {
   evt.stopPropagation();
   evt.preventDefault();
   $("#drop_zone").css("background-color", '');
+<<<<<<< HEAD
 }
 
 function addInputToMap(result) {
+=======
+>>>>>>> origin/master
 
 }
