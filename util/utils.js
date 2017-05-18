cb680bb6b4ce26b4512771b04c078cd28a224997
Sebastian Hancock <sebastian.hancock@uky.edu>
2017-05-18 18:00:38 -0400

    update

--- util/utils.js
+++ util/utils.js
@@ -2,34 +2,67 @@ function handleFileSelect(evt) {
   evt.stopPropagation();
   evt.preventDefault();
 
-  var files = evt.dataTransfer.files; // FileList object.
+  var files = evt.dataTransfer.files, // FileList object.
+    file_ext,
+    ref,
+    file_type,
+    // get name, type, size
+    output = [];
 
-  // files is a FileList of File objects. List some properties.
-  var output = [];
   for (var i = 0, f; f = files[i]; i++) {
-    output.push(f.name, f.type, f.size);
+    file_type = f.type;
+    if (file_type === '') {
+      ref = f.name.split('.');
+      file_ext = ref[ref.length - 1];
+      if (file_ext === 'json') {
+        file_type = 'application/json';
+      }
+    }
+
+    output.push(f.name, file_type, f.size);
   }
 
-  document.getElementById('list').innerHTML = output; //.join('');
-
+  //read file and send to server
   var fr = new FileReader();
 
   fr.readAsText(files[0]);
 
   fr.onload = function(e) {
-    //console.log(e);
-    var result = JSON.parse(e.target.result);
-    var formatted = JSON.stringify(result, null, 2);
-    console.log(result);
-    console.log('----------------------');
-    console.log(e.target.result);
-    console.log('----------------------');
-    console.log(formatted);
+    //console.log(e.target.result);
+
+    var result = e.target.result;
+
+    //var result = JSON.parse(e.target.result);
+
+    var validatGeoJson = geojsonhint.hint(result);
+
+    if (validatGeoJson.length > 0) {
+
+      var message = validatGeoJson.map(function(error) {
+        return 'Line ' + error.line + ': ' + error.message;
+      });
+      $('#error-header').show();
+      $('#error-message').html(message);
+    } else {
+      var formatted = JSON.stringify(result, null, 2);
+      console.log(result);
+
+      $.ajax({
+        type: 'POST',
+        url: '/upload',
+        json: true,
+        data: {
+          'fileName': output[0],
+          'fileInput': result
+        },
+        success: function(data) {
+          console.log(result);
+          console.log('upload successful!');
+        }
+      });
+    }
+    document.getElementById('list').innerHTML = output.join(',<br> ');
   };
-
-
-
-
 }
 
 function handleDragOver(evt) {
@@ -44,5 +77,8 @@ function handleDragLeave(evt) {
   evt.stopPropagation();
   evt.preventDefault();
   $("#drop_zone").css("background-color", '');
+}
+
+function addInputToMap(result) {
 
 }
